d8d839fa2d0d17b75a32b9fb9fd504a4a61fef30
GretaGur <33196263+GretaGur@users.noreply.github.com>
2018-01-04 17:55:25 +0200

    Something is wrong

---
+++ js/index.js
@@ -0,0 +1,25 @@
+const left = document.querySelector('.left');
+const right = document.querySelector('.right');
+const center = document.querySelector('.center');
+const container = document.querySelector('.container');
+
+left.addEventListener('mouseenter', () => {
+    container.classList.add('hover-left');
+});
+left.addEventListener('mouseleave', () => {
+    container.classList.remove('hover-left');
+});
+
+center.addEventListener('mouseenter', () => {
+    container.classList.add('hover-center');
+});
+center.addEventListener('mouseleave', () => {
+    container.classList.remove('hover-center');
+});
+
+right.addEventListener('mouseenter', () => {
+    container.classList.add('hover-right');
+});
+right.addEventListener('mouseleave', () => {
+    container.classList.remove('hover-right');
+});
